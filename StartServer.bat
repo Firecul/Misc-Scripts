082b95526446b45c92cfa6245257489f3350d8b0
firecul <firecul666@gmail.com>
2019-06-20 16:39:32 +0100

    Added start server script

---
+++ StartServer.bat
@@ -0,0 +1,4 @@
+del C:\FiveMServer\cfx-server-data-master\cache\files\*
+rd /S /Q C:\FiveMServer\cfx-server-data-master\cache\files\*
+cd /d C:\FiveMServer\cfx-server-data-master
+C:\FiveMServer\run.cmd +exec server.cfg
