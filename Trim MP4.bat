04e00e49c8f95c07405849af348628809bb52a47
Firecul <Firecul666@gmail.com>
2020-12-05 02:27:03 +0000

    Trim MP4
    Seemingly better fixes start on keyframe and not have the file longer than the video

--- Trim MP4.bat
+++ Trim MP4.bat
@@ -13,7 +13,7 @@ SET /P dof=Delete original [y/N]:
 if "%dof%" == "" (
 	SET dof=N
 )
-ffmpeg.exe -ss %start_time% -i %src% -t %duration% -c copy -map 0 %dest% -hide_banner
+ffmpeg.exe -ss %start_time% -i %src% -t %duration% -avoid_negative_ts make_zero -c copy -map 0 %dest% -hide_banner
 
 if "%dof%" == "y" goto delorg
 if "%dof%" == "Y" goto delorg
