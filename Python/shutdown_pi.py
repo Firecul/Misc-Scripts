c188f7d5c0a688ebd491d63a4e6eae5e0b6ca489
Firecul <Firecul666@gmail.com>
2024-06-27 02:16:58 +0100

    Update shutdown
    Uses spaces instead of tabs to be consistent with my others

--- Python/shutdown_pi.py
+++ Python/shutdown_pi.py
@@ -1,21 +1,23 @@
 #!/bin/python
-#Shutdown script, obviously
+# Shutdown script, obviously
 
 import RPi.GPIO as GPIO
 import time
 import os
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(21, GPIO.IN, pull_up_down = GPIO.PUD_UP)
+GPIO.setup(21, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
 
 # setup shutdown function
 def Shutdown(channel):
-#	os.system("sudo /etc/init.d/kismet stop")
-#	os.system("/bin/sleep 15")
-	os.system("sudo shutdown -P 1")
+    #	os.system("sudo /etc/init.d/kismet stop")
+    #	os.system("/bin/sleep 15")
+    os.system("sudo shutdown -P 1")
+
 
 # Call when pressed
-GPIO.add_event_detect(21, GPIO.FALLING, callback = Shutdown, bouncetime = 2000)
+GPIO.add_event_detect(21, GPIO.FALLING, callback=Shutdown, bouncetime=2000)
 
 while 1:
-	time.sleep(1)
+    time.sleep(1)
