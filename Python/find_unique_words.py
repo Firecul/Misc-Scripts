b7e247c10650f41c211ea5b591db4b6c37b5c7ff
Firecul <Firecul666@gmail.com>
2024-06-27 01:42:44 +0100

    Add difference finder
    Will compare two files and see what words are in one but not the other.
    Files should be arranged in alphabetical order.
    First input file should be the larger

---
+++ Python/find_unique_words.py
@@ -0,0 +1,13 @@
+﻿def find_unique_words(output_file, minus_file, difference_file):
+    with open(output_file, 'r') as f_output, open(minus_file, 'r') as f_minus, open(difference_file,
+                                                                                    'w') as f_difference:
+        output_words = set(f_output.read().split())
+        minus_words = set(f_minus.read().split())
+        unique_words = output_words - minus_words
+        f_difference.write("\n".join(unique_words))
+
+
+output_file = "output.txt"
+minus_file = "minus.txt"
+difference_file = "difference.txt"
+find_unique_words(output_file, minus_file, difference_file)
